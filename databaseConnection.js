25c87a762b6843e939c11b5b86eef6bf2443717e
hllowrld <ira.bcit@protonmail.com>
2021-03-10 21:06:49 -0800

    second commit

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,9 +4,9 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "b1ab7fb2ee03bc",
-	password: "2a484a2d",
-	database: "heroku_3d208ad4bd6f421",
+	user: "b2ea21ad3f4267",
+	password: "d85b69da",
+	database: "heroku_a7c68cae9d1b769",
 	multipleStatements: false
 };
 
